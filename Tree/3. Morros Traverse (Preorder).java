b8391fe0192ba0d7fc60950caa2101ca44e8f053
Suraj <sharmasurajkumar4649@gmail.com>
2022-09-23 16:42:16 +0530

    add Tree Question

--- Tree/3. Morros Traverse (Preorder).java
+++ Tree/3. Morros Traverse (Preorder).java
@@ -1,46 +1,45 @@
-/* Morr's order travesrsal*/
+ /* Morr's order travesrsal*/
     
-public List<Integer> preorderTraversal(TreeNode root)
-{
-    ArrayList<Integer> ans=new ArrayList<>();
-     //step-1
-     TreeNode curr=root;  
-     
-     //step 2
-     while(curr!=null)
-     {
-         //cheking curr.left!=null then create temp
-      if(curr.left!=null)
-       {
-             ans.add(curr.val);
-             TreeNode temp=curr.left;
+ public List<Integer> preorderTraversal(TreeNode root)
+ {
+     ArrayList<Integer> ans=new ArrayList<>();
+      //step-1
+      TreeNode curr=root;   
+                                                                          
+      //step 2
+      while(curr!=null)
+      {
          
-         //step-3
-         while(temp.right!=null && temp.right!=curr)
-         {
-             temp=temp.right;
-         }
-         if(temp.right==null) // means links note present then make link
-         {
-             ans.add(curr.val);
-             temp.right=curr;
-             curr=curr.left;
-         }
-         else  // remove the link
-         { 
-             // Links already present
-            
-             temp.right=null;
-             curr=curr.right;
-         }
+          //cheking curr.left!=null then create temp
+       if(curr.left!=null)
+        {   
+              TreeNode temp=curr.left;
+          
+          //step-3
+          while(temp.right!=null && temp.right!=curr)
+          {
+              temp=temp.right;
+          }
+          if(temp.right==null) // means links note present then make link
+          {
+              ans.add(curr.val);
+              temp.right=curr;
+              curr=curr.left;
+          }
+          else  // remove the link
+          { 
+              // Links already present
+             
+              temp.right=null;
+              curr=curr.right;
+          }
+       }
+      else
+      {
+          ans.add(curr.val);
+          curr=curr.right;
       }
-     else
-     {
-         ans.add(curr.val);
-         curr=curr.right;
-     }
-   }
-   
-   return ans;
-}
-
+    }
+    
+    return ans;
+ }
