28d282f93da16730effe4b4bc61be6fc9d39bb71
SurajSharmaCSE <sharmasurajkumar4649@gmail.com>
2023-11-23 11:52:25 +0530

    some change in Graph

--- Graph/2. Cycle detection/2. detect-cycle-in-a-directed-graph.java
+++ Graph/2. Cycle detection/2. detect-cycle-in-a-directed-graph.java
@@ -52,6 +52,65 @@ class Solution {
 /* -------------------------Start bfs Code --------------------- */
 
 // use alos kahn's algo for find cycle in directed graph 
+ // Function to detect cycle in a directed graph.
+public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) 
+   {
+        // code here
+        /*-- BFS  ans using topological sort-------*/
+         Queue<Integer> q = new LinkedList<>();
+         
+         //step-1 find Indegree
+         int indegree[]=new int[V];
+         for(int i=0;i<V;i++)
+         {
+             for(Integer e:adj.get(i))
+             {
+                 indegree[e]++;
+             }
+         }
+         
+         //step-2 add Nodes in Queue Whose Indegree is zero alreday 
+         for(int i=0;i<V;i++)
+         {
+             if(indegree[i]==0)
+             
+             {
+                 q.add(i);
+             }
+         }
+         int count =0;
+         //step-4 Apply Noram BFS
+         int ans[] = new int[V];
+         int i=0;
+         while(q.size()>0)
+         {
+             count++ ;
+             //1
+             int node=q.remove();
+             //2
+             ans[i]=node;
+             i++;
+             //3 reduce the indegree
+             for(Integer e: adj.get(node))
+             {
+                 indegree[e]--;
+                 if(indegree[e]==0)
+                 {
+                     q.add(e);
+                 }
+             }
+         }
+         if(count==V)
+         {
+             return false;
+         }
+         else 
+         {
+             return true;
+         }
+        
+    
+    }
 
 /*Complete the function below*/
 
