205f9647b656327f21f800af4616396c947d901c
SurajSharmaCSE <sharmasurajkumar4649@gmail.com>
2024-01-07 12:48:54 +0530

    some DP questions add

--- DP/7_longest-palindromic-subsequence.java
+++ DP/7_longest-palindromic-subsequence.java
@@ -44,4 +44,31 @@ public static int lps(String s,int i,int j,int dp[][])
         {
             return dp[i][j]=Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
         }
+
+        //Tabulation -> Gap Method
+        public static int Tabulation(String s)
+        {
+        int n = s.length();
+        int dp[][] = new int[n][n];
+
+        for(int gap=0;gap<n;gap++)
+        {
+            for(int i=0,j=gap;j<n;i++,j++)
+            {
+                if(i>=j)
+                {
+                    dp[i][j] = (i==j)?1:0;
+                }
+                else if(s.charAt(i)==s.charAt(j))
+                {
+                    dp[i][j] = dp[i+1][j-1]+2;
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];  
+    }
     }
